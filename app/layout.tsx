67e8b26418466d92f3e0e47dd82af3a382aaecc3
OssFit <oalatrista@gmail.com>
2023-03-28 16:42:57 -0500

    favicon & tile

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 
 
@@ -13,6 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+          <link rel="image/x-icon" href="favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="logo.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="logo.png"/>
+</head>
+      
+      <title>Meal Genius</title>
       <body><ChakraProvider>
         <Navbar/>
         {children}
